5362d4feb9656e3a59d9eba7ec5628c35674c9c6
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-05 22:34:16 +0400

    ASP.NET Core Web API .NET 8 2024 - 12. Comment System

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,19 @@
+﻿using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Mappers;
+
+public static class CommentMapper
+{
+    public static CommentDto ToCommentDto(this Comment comment)
+    {
+        return new CommentDto()
+        {
+            Id = comment.Id,
+            Title = comment.Title,
+            Content = comment.Content,
+            CreatedOn = comment.CreatedOn,
+            StockId = comment.StockId,
+        };
+    }
+}
---
+++ api/Repository/CommentRepository.cs
@@ -0,0 +1,53 @@
+﻿using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository;
+
+public class CommentRepository : ICommentRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CommentRepository(ApplicationDbContext applicationDbContext)
+    {
+        _context = applicationDbContext;
+    }
+
+    public async Task<List<Comment>> GetAllAsync()
+    {
+        return await _context.Comments.ToListAsync();
+    }
+
+    public async Task<Comment?> GetByIdAsync(int id)
+    {
+        return await _context.Comments.FindAsync(id);
+    }
+
+    public async Task<Comment> CreateAsync(Comment comment)
+    {
+        await _context.Comments.AddAsync(comment);
+        await _context.SaveChangesAsync();
+        return comment;
+    }
+
+    public Task<Comment?> UpdateAsync(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Comment?> DeleteAsync(int id)
+    {
+        var comment = await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (comment == null)
+        {
+            return null;
+        }
+
+        _context.Comments.Remove(comment);
+        await _context.SaveChangesAsync();
+
+        return comment;
+    }
+}
