4043660d1253d2b176cb9eed6075555cd468ceea
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-05 16:21:27 +0400

    ASP.NET Core Web API .NET 8 2024 - 7. PUT (Update)

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -45,4 +45,26 @@ public class StockController : ControllerBase
         _context.SaveChanges();
         return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
     }
+
+    [HttpPut("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] CreateStockRequestDto updateDto)
+    {
+        var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+
+        stockModel.Symbol = updateDto.Symbol;
+        stockModel.CompanyName = updateDto.CompanyName;
+        stockModel.Purchase = updateDto.Purchase;
+        stockModel.LastDiv = updateDto.LastDiv;
+        stockModel.Industry = updateDto.Industry;
+        stockModel.MarketCap = updateDto.MarketCap;
+
+        _context.SaveChanges();
+        
+        return Ok(stockModel.ToStockDto());
+    }
 }
