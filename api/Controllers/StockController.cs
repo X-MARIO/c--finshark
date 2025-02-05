84b82809bcff2ea6165264c405878aa78ca248cb
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-05 16:35:07 +0400

    ASP.NET Core Web API .NET 8 2024 - 8. DELETE

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -64,7 +64,24 @@ public class StockController : ControllerBase
         stockModel.MarketCap = updateDto.MarketCap;
 
         _context.SaveChanges();
-        
+
         return Ok(stockModel.ToStockDto());
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+
+        _context.Stocks.Remove(stockModel);
+        
+        _context.SaveChanges();
+
+        return NoContent();
     }
 }
