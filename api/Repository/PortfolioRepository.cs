c8a4d584a41b9341aa8f299ddb2cc2bf6b50f9d9
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-09 23:08:58 +0400

    ASP.NET Core Web API .NET 8 2024 - 26. Portfolio GET

---
+++ api/Repository/PortfolioRepository.cs
@@ -0,0 +1,30 @@
+﻿using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository;
+
+public class PortfolioRepository : IPortfolioRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public PortfolioRepository(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<List<Stock>> GetPortfolios(AppUser user)
+    {
+        return await _applicationDbContext.Portfolios.Where(u => u.AppUserId == user.Id)
+            .Select(stock => new Stock
+            {
+                Id = stock.StockId,
+                Symbol = stock.Stock.Symbol,
+                CompanyName = stock.Stock.CompanyName,
+                Purchase = stock.Stock.Purchase,
+                Industry = stock.Stock.Industry,
+                MarketCap = stock.Stock.MarketCap,
+            }).ToListAsync();
+    }
+}
