bee1d34f50302a583c531ccb5682251afc1d6562
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-09 21:35:57 +0400

    ASP.NET Core Web API Identity JWT 2024 - 23. Token Service

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,12 @@ namespace api.Controllers;
 public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
+    private readonly ITokenService _tokenService;
 
-    public AccountController(UserManager<AppUser> userManager)
+    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
     {
         _userManager = userManager;
+        _tokenService = tokenService;
     }
 
     [HttpPost("register")]
@@ -37,7 +40,14 @@ public class AccountController : ControllerBase
             if (createUser.Succeeded)
             {
                 var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
-                return roleResult.Succeeded ? Ok("User created") : StatusCode(500, roleResult.Errors);
+                return roleResult.Succeeded
+                    ? Ok(new NewUserDto
+                    {
+                        Username = appUser.UserName,
+                        Email = appUser.Email,
+                        Token = _tokenService.CreateToken(appUser)
+                    })
+                    : StatusCode(500, roleResult.Errors);
             }
             else
             {
