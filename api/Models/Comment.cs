91b3f62c3ef54751a9106f51cb04303c9d2a2ea9
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-10 09:55:29 +0400

    add CommentOneToOne migration

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -8,13 +8,17 @@ public class Comment
     public int Id { get; set; }
 
     public string Title { get; set; } = string.Empty;
-    
+
     public string Content { get; set; } = string.Empty;
 
     public DateTime CreatedOn { get; set; } = DateTime.Now.ToUniversalTime();
-    
+
     public int? StockId { get; set; }
     // Navigation
 
     public Stock? Stock { get; set; }
+
+    public string AppUserId { get; set; } = string.Empty;
+
+    public AppUser? AppUser { get; set; }
 }
