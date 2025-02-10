6091ab6711f662000c7ea98adb482e71a2cd7b2c
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-10 09:32:22 +0400

    Bug fix

--- api/Extensions/ClaimsExtensions.cs
+++ api/Extensions/ClaimsExtensions.cs
@@ -6,6 +6,6 @@ public static class ClaimsExtensions
 {
     public static string GetUsername(this ClaimsPrincipal user)
     {
-        return user.Claims.SingleOrDefault(c => c.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givename"))?.Value;
+        return user.Claims.SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")).Value;
     }
 }
